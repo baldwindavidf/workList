4fc07c1771f640fca959c896d61661c4321db507
baldwindavidf <sourcedevelopers@protonmail.com>
2018-11-04 16:45:49 -0700

    u

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,4 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
@@ -19,8 +10,21 @@ for (i = 0; i < close.length; i++) {
   }
 }
 function setUpPage() {
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
+//var list = document.querySelector('ul');
+var list = document.getElementById('myUL')
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
