bd721129e9f674e402a477f85c0daff5eafba836
baldwindavidf <sourcedevelopers@protonmail.com>
2018-11-23 19:40:10 -0700

    u

--- index.php
+++ index.php
@@ -67,10 +67,10 @@ header("Location:index.php");
       
       <div id="menu1" class="col-3 col-s-3 menu">
         <ul>
-          <li touch="myFontOne();" class="uList1">MOBIL TOUCH 1</li>
-          <li touch="myFontTwo();" class="uList2">MOBIL TOUCH 2</li>
-          <li touch="myFontThree();" class="uList3">MOBIL TOUCH 3</li>
-          <li touch="myFontFour();" class="uList4">MOBIL TOUCH 4</li>
+          <li ontouchstart="myFontOne();" class="uList1">MOBIL TOUCH 1</li>
+          <li ontouchstart="myFontTwo();" class="uList2">MOBIL TOUCH 2</li>
+          <li ontouchstart="myFontThree();" class="uList3">MOBIL TOUCH 3</li>
+          <li ontouchstart="myFontFour();" class="uList4">MOBIL TOUCH 4</li>
         </ul>
       </div>
       
